d9dbb5087d1124a7109df8bfb0cba33942cdef1f
Joao Pedro <joao.fpsilva98@gmail.com>
2023-11-26 15:21:43 -0300

    increment more funcionalities to components

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,6 +1,17 @@
-export default function ResultModal({result,targetTime}){
+import {forwardRef, useImperativeHandle, useRef} from "react"
+
+export default forwardRef(function ResultModal({result,targetTime}, ref){
+    const dialog = useRef()
+
+    useImperativeHandle(ref, () => {
+        return {
+            open(){
+                dialog.current.showModal();
+            }
+        }
+    })
     return (
-        <dialog className="result-modal" open>
+        <dialog  ref={dialog} className="result-modal" >
             <h2>You {result}</h2>
             <p>
                 The target time was <strong>{targetTime} seconds.</strong>
@@ -14,4 +25,4 @@ export default function ResultModal({result,targetTime}){
         </dialog>
     )
 
-}
+})
